e9d20e49e07ebc63cd6759faf255a374434c8d5a
Kim Hjort <khm@inlogic.dk>
2020-03-05 14:24:47 +0100

    sync to github

--- SyncToGithub/syncgithub.sh
+++ SyncToGithub/syncgithub.sh
@@ -1,10 +1,10 @@
 git clone --bare https://kimhjort.visualstudio.com/BaconIpsumGen/_git/BaconIpsumGen
 # Makes a bare clone of the external repository in a local directory
 
-cd BaconIpsumGen
+cd BaconIpsumGen.git
 git push --mirror https://github.com/xobsdk/baconipsum-nuget.git
 # Pushes the mirror to the new GitHub repository
 
 cd ..
-rm -rf BaconIpsumGen
+rm -rf BaconIpsumGen.git
 # Removes cloned repo
