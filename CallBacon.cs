ac66f191773335ac2c52198d7de2c10edfec47a3
Kim Hjort <khm@inlogic.dk>
2020-03-05 11:21:11 +0100

    initial commit

---
+++ CallBacon.cs
@@ -0,0 +1,25 @@
+public static string CallBacon (int paras, int sentences) {
+    string urlParas = "";
+
+    if (paras > 0) {
+        urlParas = "paras=" + paras;
+    }
+
+    if (sentences > 0) {
+        urlParas = "sentences=" + sentences;
+    }
+
+    string html = string.Empty;
+    string url = @"http://baconipsum.com/api/?type=all-meat&" + urlParas + "&start-with-lorem=0&format=text";
+
+    HttpWebRequest request = (HttpWebRequest) WebRequest.Create (url);
+    request.AutomaticDecompression = DecompressionMethods.GZip;
+
+    using (HttpWebResponse response = (HttpWebResponse)request.GetResponse ())
+    using (Stream stream = response.GetResponseStream())
+    using (StreamReader reader = new StreamReader(stream)) {
+        html = reader.ReadToEnd ();
+    }
+
+    return html;
+}
